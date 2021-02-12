6d21fff6e3af772a7c2e39bd71cefe1c4d48e31a
laputaqsh <qsh0911@qq.com>
2021-02-12 14:52:13 +0800

    best wishes

--- src/test/java/qsh/laputa/AutoFillTests.java
+++ src/test/java/qsh/laputa/AutoFillTests.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import qsh.laputa.entity.User;
 import qsh.laputa.mapper.UserMapper;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
