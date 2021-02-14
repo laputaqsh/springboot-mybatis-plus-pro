8d31ba30f34ea48109f7b04c4dc3181875b4c748
bjunicom <qsh0911@qq.com>
2021-02-14 10:01:28 +0800

    optlocker final

--- src/test/java/qsh/laputa/OptLockerTests.java
+++ src/test/java/qsh/laputa/OptLockerTests.java
@@ -1,5 +1,7 @@
 package qsh.laputa;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +27,25 @@ public class OptLockerTests {
 
     @Test
     public void update() {
-        int version = 1;
+        QueryWrapper<User> queryWrapper = Wrappers.query();
+
+        int version = 2;
         User user = new User();
         user.setEmail("zqg@baomidou.com");
-        user.setId(1360116789345845250L);
-        //user.setVersion(version);
-        int rows = userMapper.updateById(user);
+        user.setVersion(version);
+        queryWrapper.eq("name", "张琪格");
+
+        int rows = userMapper.update(user, queryWrapper);
         System.out.println("更新数：" + rows);
+
+        /*//Wrapper不能复用，会出现sql语句逻辑混乱的情况
+        int version2 = 3;
+        User user2 = new User();
+        user2.setEmail("z7g@baomidou.com");
+        user2.setVersion(version2);
+        queryWrapper.eq("age", 26);
+
+        int rows2 = userMapper.update(user2, queryWrapper);
+        System.out.println("更新数2：" + rows2);*/
     }
 }
